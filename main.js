3fd47e2dfe4303704269be57099117f211dbb1fa
andruuww <0427andrewnguyen@gmail.com>
2023-08-14 17:05:50 -0700

    code was behaving asynchronously? created problems, fixed

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 $(document).ready(() => {
-    const form = document.getElementById("form");
-
     const qrcode = new QRCode("data", {
         text: "TEAM949",
         width: 256, 
@@ -13,7 +11,21 @@ $(document).ready(() => {
     $("#output").hide();
 
     $("form").on('submit', (e) => {
+        // code was behaving asynchronously? created problems
+        processData();
+        e.preventDefault();
+    });
+
+    function generateCode(data) {
+        return new Promise((resolve, reject) => {
+            qrcode.makeCode(data);
+            resolve();
+        })
+    }
+
+    async function processData() {
         qrcode.clear();
+        $("#output").show();
     
         let data = "";
         const teamNumber = $("form :input[id=teamNumber]").val();
@@ -23,31 +35,29 @@ $(document).ready(() => {
                 // skip the button
                 return true;
             }
-            data += element.value + ",";
+            data += element.value.toLowerCase() + ",";
         });
-    
-        qrcode.makeCode(data);
-        $("#team-label").html("FRC#" + teamNumber);
+        
+        await generateCode(data);
 
-        $("#output").show();
+        $("#team-label").html(`FRC# ${teamNumber}`);        
         // downloading the resulting qr code and meta label
         html2canvas(document.getElementById("output")).then((canvas) => {
             // IE/Edge Support... idk if this actually works but let's just say it works
             if (window.navigator.msSaveBlob) {
-                window.navigator.msSaveBlob(canvas.msToBlob(), "Pitscouting FRC#" + teamNumber + ".png");
+                window.navigator.msSaveBlob(canvas.msToBlob(), `Pitscouting FRC#${teamNumber}.png`);
             } else {
                 const a = document.createElement("a");
 
                 document.body.appendChild(a);
                 a.href = canvas.toDataURL();
-                a.download = "PitScouting FRC#" + teamNumber + ".png";
+                a.download = `Pitscouting FRC#${teamNumber}.png`;
                 a.click();
                 document.body.removeChild(a);
             }
         });
 
         $("#output").hide();
-
-        e.preventDefault();
-    });
+    }
 });
+
